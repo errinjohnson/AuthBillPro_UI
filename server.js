febca43be633f5e2625c5dde43a25370230a3911
Errin Johnson <errin.connects@gmail.com>
2024-09-30 18:34:22 -0500

    working on issue of invalid domain, updated cookie session info

--- server.js
+++ server.js
@@ -18,37 +18,37 @@ app.use(express.json()); // Parse JSON bodies
 app.use(cors({
 
     origin: 'https://plankton-app-2-9k8uf.ondigitalocean.app' // Replace with your actual front-end domain
-  
-  }));
-  
-  
-  // Set up express-session middleware
-  
-  app.use(session({
-  
-    secret: process.env.SESSION_SECRET, // Use a strong secret from your environment
-  
-    resave: false,
-  
-    saveUninitialized: false,
-  
-    cookie: {
-  
-      domain: '.https://plankton-app-2-9k8uf.ondigitalocean.app', // Set the cookie domain to your root domain
-  
-      path: '/',
-  
-      secure: true, // Use HTTPS
-  
-      httpOnly: true,
-  
-      sameSite: 'Lax',
-  
-    }
-  
-  }));
 
-// Serve static files from the 'public' directory
+}));
+
+
+// Set up express-session middleware
+
+app.use(session({
+
+    secret: process.env.SESSION_SECRET, // Use a strong secret from your environment
+
+    resave: false,
+
+    saveUninitialized: false,
+
+    cookie: {
+
+        domain: '.plankton-app-2-9k8uf.ondigitalocean.app', // Set the cookie domain to your root domain
+
+        path: '/',
+
+        secure: true, // Use HTTPS
+
+        httpOnly: true,
+
+        sameSite: 'Lax',
+
+    }
+
+}));
+  
+ // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Create a MySQL connection
