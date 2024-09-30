44c0e0b8e5f145b791e01933cce1f63152c1e624
Errin Johnson <errin.connects@gmail.com>
2024-09-30 17:42:48 -0500

    trying to fix cookies invalid domain issue

--- server.js
+++ server.js
@@ -14,6 +14,39 @@ app.use(cors()); // Enable CORS for all origins
 
 app.use(express.json()); // Parse JSON bodies
 
+app.use(cors({
+
+    origin: 'https://plankton-app-2-9k8uf.ondigitalocean.app' // Replace with your actual front-end domain
+  
+  }));
+  
+  
+  // Set up express-session middleware
+  
+  app.use(session({
+  
+    secret: process.env.SESSION_SECRET, // Use a strong secret from your environment
+  
+    resave: false,
+  
+    saveUninitialized: false,
+  
+    cookie: {
+  
+      domain: '.https://plankton-app-2-9k8uf.ondigitalocean.app', // Set the cookie domain to your root domain
+  
+      path: '/',
+  
+      secure: true, // Use HTTPS
+  
+      httpOnly: true,
+  
+      sameSite: 'Lax',
+  
+    }
+  
+  }));
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
