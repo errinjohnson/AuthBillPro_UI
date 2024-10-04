8c2ec93e81c46f7611ae9cf9a0a6102d56ccfc0e
Errin Johnson <errin.connects@gmail.com>
2024-10-03 19:31:30 -0500

    working on edit function, retrieve single participant

--- routes/participantRoutes.js
+++ routes/participantRoutes.js
@@ -16,7 +16,19 @@ module.exports = (mysqlConnection) => { // Accept mysqlConnection as a parameter
             res.json(results);
         });
     });
-
+    router.get('/:id', (req, res) => {
+        const query = 'SELECT * FROM participants WHERE participant_id = ?';
+        mysqlConnection.query(query, [req.params.id], (err, results) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            if (results.length === 0) {
+                return res.status(404).json({ message: 'Participant not found' });
+            }
+            res.json(results[0]);
+        });
+    });
+    
     // POST /api/participants
     router.post('/', (req, res) => {
         const { email, first_name, last_name, phone, registration } = req.body;
