c76769a99459992923b5301cf85930bec9139da9
Errin Johnson <errin.connects@gmail.com>
2024-10-06 19:14:27 -0500

    updated auth urls and routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ module.exports = (mysqlConnection) => {
 
     // Get all authorizations
     router.get('/', (req, res) => {
-        const query = 'SELECT * FROM Authorizations';
+        const query = 'SELECT * FROM auth';
         mysqlConnection.query(query, (error, results) => {
             if (error) return res.status(500).json({ error });
             res.json(results);
@@ -26,7 +26,7 @@ module.exports = (mysqlConnection) => {
     router.post('/', (req, res) => {
         const { auth_number, auth_begin_date, auth_end_date, auth_rate, auth_billable_hours, auth_remaining_billable_hours } = req.body;
         const query = `
-            INSERT INTO Authorizations (auth_number, auth_begin_date, auth_end_date, auth_rate, auth_billable_hours, auth_remaining_billable_hours)
+            INSERT INTO auth (auth_number, auth_begin_date, auth_end_date, auth_rate, auth_billable_hours, auth_remaining_billable_hours)
             VALUES (?, ?, ?, ?, ?, ?)
         `;
         mysqlConnection.query(query, [auth_number, auth_begin_date, auth_end_date, auth_rate, auth_billable_hours, auth_remaining_billable_hours], (error, results) => {
@@ -39,7 +39,7 @@ module.exports = (mysqlConnection) => {
     router.put('/:auth_number', (req, res) => {
         const { auth_begin_date, auth_end_date, auth_rate, auth_billable_hours, auth_remaining_billable_hours } = req.body;
         const query = `
-            UPDATE Authorizations SET auth_begin_date = ?, auth_end_date = ?, auth_rate = ?, auth_billable_hours = ?, auth_remaining_billable_hours = ?
+            UPDATE auth SET auth_begin_date = ?, auth_end_date = ?, auth_rate = ?, auth_billable_hours = ?, auth_remaining_billable_hours = ?
             WHERE auth_number = ?
         `;
         mysqlConnection.query(query, [auth_begin_date, auth_end_date, auth_rate, auth_billable_hours, auth_remaining_billable_hours, req.params.auth_number], (error, results) => {
